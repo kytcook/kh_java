299bf702b9d2c2325668a23edf345de65e667362
Teak <98031858+kytcook@users.noreply.github.com>
2022-05-16 00:39:34 +0900

    Update Gu9danSimulation.java

--- src/homework/Gu9danSimulation.java
+++ src/homework/Gu9danSimulation.java
@@ -1,10 +1,9 @@
-package homework;								// 패키지
+package homework;					// 패키지
 
-public class Gu9danSimulation {					// class GugudanSimulation
+public class Gu9danSimulation {				// class GugudanSimulation
 	public static void main(String[] args) {   	// main method
-		Gu9dan g9; 								// Gu9dan의 객체변수?? 타입?? 선언??
-		g9 = new Gu9dan();						// 객체 생성? 객체 호출?? 인스턴스화??
-		
-	} //////////////////////////////////////////[ end of method main]
+		Gu9dan g9; 				// Gu9dan의 객체변수?? 타입?? 선언??
+		g9 = new Gu9dan();			// 객체 생성? 객체 호출?? 인스턴스화??
+	} //////////////////////////////////////////////[ end of method main]
 
-} //////////////////////////////////////////////[ end of class Gu9danSimulation ] 
+} //////////////////////////////////////////////////////[ end of class Gu9danSimulation ] 
