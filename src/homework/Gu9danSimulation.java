f969862581aa064085aae7c9f1e6197cc48591dc
kytcook <kytcook@naver.com>
2022-05-16 00:34:17 +0900

    backUp_2022.05.16.12:34

--- src/homework/Gu9danSimulation.java
+++ src/homework/Gu9danSimulation.java
@@ -1,11 +1,10 @@
-package homework;
+package homework;								// 패키지
 
-public class Gu9danSimulation {
-
-	public static void main(String[] args) {
-		Gu9dan g9 = new Gu9dan();
+public class Gu9danSimulation {					// class GugudanSimulation
+	public static void main(String[] args) {   	// main method
+		Gu9dan g9; 								// Gu9dan의 객체변수?? 타입?? 선언??
+		g9 = new Gu9dan();						// 객체 생성? 객체 호출?? 인스턴스화??
 		
-				
-	}
+	} //////////////////////////////////////////[ end of method main]
 
-}
+} //////////////////////////////////////////////[ end of class Gu9danSimulation ] 
