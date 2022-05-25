32a95eb0d658ed674cc4e082c81b5bcd17f7a857
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-05-25 15:09:12 +0900

    backup_20220525_1510

---
+++ src/getlist/DeptApp.java
@@ -0,0 +1,28 @@
+package getlist;
+
+import java.sql.Connection;
+
+public class DeptApp {
+	
+	void Deptapp() {
+		getDeptList() : List<map<String, Object>> // ip번호 포트번호
+		
+	}
+	class.forName(oracle.jdbc.driver.OracleDriver ); // 오라클 밑에 jdbc밑에 드라이버밑에 오라클드라이버 
+	//ojdbc.jar 제조사가 안에있다. 벤더
+	Connection con = DriverManager.getConnetion(URL, User, pw) // 접속한다. 통로. 채널
+	PreparedStatment pstmt = con.prepareStatement(Select, dept)// 전령을 보낸다. Select에서 dept까지 오라클로 보낸다
+	ResultSet rs = pstmt.executQuery()	//오라클에게 select문 처리를 맡긴다.
+	While(rs.next())
+	// while문 안에서는 map을 4번 인스턴스화 해야 한다. 가로가로가로가로
+	
+	
+			
+			
+	try catch
+	public static void main(String[] args) {
+
+		
+	}
+
+}
