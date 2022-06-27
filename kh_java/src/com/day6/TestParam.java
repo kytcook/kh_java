2a5da9faac15264c08b433494513109d73e4afe5
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-06-27 15:19:25 +0900

    TomcatConnection_study_220627

--- kh_java/src/com/day6/TestParam.java
+++ kh_java/src/com/day6/TestParam.java
@@ -1,23 +1,25 @@
 package com.day6;
-
+// ECMAScript6 -> class지원하게 됨
 class Param{
+	// 생성자가 전변에 대한 초기화를 대신 해줌
 	int ival; // 전번 초기화 생략 가능 - 0
 }
 
 public class TestParam {
-	void effectParam(Param p) {
-		p = new Param(); // 18번과 9번의 Param은 다른 객체이다. 타입은 같다.
-		p.ival = 300;
+	void effectParam(Param p) { // call by reference 값에의한 호출이 아니라 참조주소에 의하 호출. 원본의 주소번지 갖고있음
+		p = new Param(); // 복사본이 생성되고 그러니까 새로운 객체가 생성됨. 타입은 같지만 객체는 다르다
+		p.ival = 300; // 원본에 ival값이 0 -> 500 -> 300 변경됨
 		System.out.println("sub ival : " + p.ival); //300
 	}	
 	public static void main(String[] args) {
 		// 내 안에 있는 정의된 메소드라 할지라도 메인 메소드 안에서(non-static → static) 호출 불가
 		// 인스턴스화를 해야 사용할 수 있다.
 		TestParam tp = new TestParam();
+		// 인스턴스화가 진행 - 메모리에 로딩됨
 		// tp.effectParam(null);
 		Param p = new Param() ;
-		p.ival	= 500;
-		tp.effectParam(p);
+		p.ival	= 500; // 지번 p에 500초기화
+		tp.effectParam(p);	// 메인에서 출력전에 메소드호출이 먼저 일어남
 		// 20번 라인의 값은 몇일까요?
 		System.out.println("main ival : " + p.ival); //500
 		
--- taek_web/src/main/webapp/json/deptToJSON.jsp
+++ taek_web/src/main/webapp/json/deptToJSON.jsp
@@ -6,15 +6,29 @@
 <%@ page import   = "com.google.gson.Gson" %>
 <%
 		//스크립트
+		// query string or query parameter -> GET방식이다, 가능, POST는 불가능하다.
+		// 전송방식 - GET(단위테스트 가능), POST 방식은 반드시 javascript의 도움이 필요함
+		String deptno = request.getParameter("deptno");
+		// out.print("요청 파마미터에서 입력 받은 값 => "+deptno); 
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		DBConnectionMgr dbMgr = new DBConnectionMgr();
 		List<Map<String,Object>> deptList = null;
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT deptno, dname,loc FROM dept");
+		if(deptno !=null) {
+			sql.append(" WHERE deptno = ?");
+		} 
+
 		try {
 			con = dbMgr.getConnection();
 			// 파라미터로 받은 select문을 전달
 			pstmt = con.prepareStatement("SELECT deptno, dname, loc FROM dept");
+			if(deptno !=null) {
+				pstmt.setString(1, deptno);
+			}
+			
 			// 전달 된 select문에 대한 처리를 요청하고 커서 받아내기
 			rs = pstmt.executeQuery();
 			deptList = new ArrayList<>();
